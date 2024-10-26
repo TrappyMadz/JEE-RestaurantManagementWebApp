5b54bbefcbf6595a16f4a5476e8527ae9a0bd12a
Charrier Simon <simon.charrier42@outlook.fr>
2024-10-26 15:31:01 +0200

    Initialisation du projet

---
+++ src/test/java/fr/cytech/restaurant_management/RestaurantManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cytech.restaurant_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
