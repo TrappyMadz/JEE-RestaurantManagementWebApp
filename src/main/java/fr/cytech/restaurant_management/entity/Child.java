abe95436c9c20af4dc6f23701e4212a35d67c8bb
Charrier Simon <simon.charrier42@outlook.fr>
2024-11-14 22:02:29 +0100

    Added adding children functionnality and added header template

--- src/main/java/fr/cytech/restaurant_management/entity/Child.java
+++ src/main/java/fr/cytech/restaurant_management/entity/Child.java
@@ -33,4 +33,28 @@ public class Child {
 		return Objects.equals(id, other.id);
 	}
 
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 }
