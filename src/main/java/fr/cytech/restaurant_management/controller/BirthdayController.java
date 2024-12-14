16f28b606282a5b6525a4825836948ddaeb58fcd
Charrier Simon <simon.charrier42@outlook.fr>
2024-12-14 18:10:18 +0100

    Started second step

--- src/main/java/fr/cytech/restaurant_management/controller/BirthdayController.java
+++ src/main/java/fr/cytech/restaurant_management/controller/BirthdayController.java
@@ -13,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import fr.cytech.restaurant_management.entity.Animatronic;
 import fr.cytech.restaurant_management.entity.Birthday;
+import fr.cytech.restaurant_management.entity.Child;
 import fr.cytech.restaurant_management.entity.Restaurant;
 import fr.cytech.restaurant_management.repository.AnimatronicRepository;
 import fr.cytech.restaurant_management.repository.BirthdayRepository;
+import fr.cytech.restaurant_management.repository.ChildRepository;
 import fr.cytech.restaurant_management.repository.RestaurantRepository;
 
 @Controller
@@ -28,6 +30,9 @@ public class BirthdayController {
 	AnimatronicRepository animatronicRepository;
 	@Autowired
 	BirthdayRepository birthdayRepository;
+	@Autowired
+	ChildRepository childRepository;
+	
 	
 	@GetMapping("/add")
 	public String initBirthday(Model model) {
@@ -52,17 +57,27 @@ public class BirthdayController {
 	
 	@PostMapping("/add")
 	public String createBirthdayPhase1(Model model, @ModelAttribute Birthday birthday) {
+		List<Restaurant> restaurants = restaurantRepository.findAll();
+		List<Animatronic> animatronics = animatronicRepository.findAll();
+		
+		model.addAttribute("restaurants",restaurants);
+		model.addAttribute("animatronics",animatronics);
 		if (birthday.getDate().compareTo(LocalDate.now()) <= 0) {
 			model.addAttribute("error", "Entrez une date valide.");
 			model.addAttribute("birthday",birthday);
 			return "birthdayForm1";
 		}
-		if (birthday.getRestaurant() == null || birthday.getAnimatronic1() == null || birthday.getAnimatronic2() == null) {
+		if (birthday.getRestaurant() == null || birthday.getAnimatronic1() == null) {
 			model.addAttribute("error", "Complétez tous les champs.");
 			model.addAttribute("birthday", birthday);
 			return "birthdayForm1";
 		}
+		if (birthday.getAnimatronic1() == birthday.getAnimatronic2()) {
+			birthday.setAnimatronic2(null);
+		}
 		model.addAttribute("birthday",birthday);
+		List<Child> enfants = childRepository.findAll();
+		model.addAttribute("children", enfants);
 		return "birthdayForm2";
 	}
 
