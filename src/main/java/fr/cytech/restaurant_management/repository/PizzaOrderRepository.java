58b4cb4f4995d41ef987a0b696f60402fe2fc0a7
MVchara <vannimarco2003@gmail.com>
2024-12-15 21:46:18 +0100

    bug fix
    no more crash when trying to delete pizza while they exist elsewhere

--- src/main/java/fr/cytech/restaurant_management/repository/PizzaOrderRepository.java
+++ src/main/java/fr/cytech/restaurant_management/repository/PizzaOrderRepository.java
@@ -1,9 +1,15 @@
 package fr.cytech.restaurant_management.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import fr.cytech.restaurant_management.entity.PizzaOrder;
 
 
 public interface PizzaOrderRepository extends JpaRepository<PizzaOrder, Long>{
+	
+	@Query("SELECT p FROM PizzaOrder p WHERE p.pizza.id=:id")
+	List<PizzaOrder> findbyPizzaId(Long id);
 }
